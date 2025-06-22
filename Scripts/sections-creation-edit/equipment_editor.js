881fdc57ece7fb461507360ea6ed31c6c580be8c
Bheatriz Urias <63936197+BheatrizUrias@users.noreply.github.com>
2025-06-22 20:00:58 -0300

    delete

--- Scripts/sections-creation-edit/equipment_editor.js
+++
@@ -1,8 +0,0 @@
-// abilities_editor.js - Versão inicial apenas para carregamento
-export function populateEquipmentSection(currentCharacter) {
-  // Por enquanto não faz nada, apenas permite o carregamento do partial
-  console.log('Seção de Atributos carregada');
-  
-  // Você pode adicionar aqui qualquer inicialização básica necessária
-  // Mas conforme solicitado, não vamos implementar a lógica ainda
-}
